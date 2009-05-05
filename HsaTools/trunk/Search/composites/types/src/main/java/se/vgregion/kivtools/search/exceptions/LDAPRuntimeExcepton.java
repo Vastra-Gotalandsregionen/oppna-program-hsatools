a5579378039ade73ae9666e7fcbfcf549761070f
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-05-05 11:40:14 +0000

    Add LDAPRuntimeException

---
+++ HsaTools/trunk/Search/composites/types/src/main/java/se/vgregion/kivtools/search/exceptions/LDAPRuntimeExcepton.java
@@ -0,0 +1,11 @@
+package se.vgregion.kivtools.search.exceptions;
+
+public class LDAPRuntimeExcepton extends RuntimeException {
+
+	public LDAPRuntimeExcepton(String message) {
+		super(message);
+	}
+
+	private static final long serialVersionUID = 1L;
+
+}
