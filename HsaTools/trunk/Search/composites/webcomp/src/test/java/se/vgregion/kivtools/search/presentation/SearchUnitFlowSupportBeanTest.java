4e60d4e4a9ffe9427281530889aa0170158060b4
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-05-05 11:56:50 +0000

    Added show person recursively

--- HsaTools/trunk/Search/composites/webcomp/src/test/java/se/vgregion/kivtools/search/presentation/SearchUnitFlowSupportBeanTest.java
+++ HsaTools/trunk/Search/composites/webcomp/src/test/java/se/vgregion/kivtools/search/presentation/SearchUnitFlowSupportBeanTest.java
@@ -17,12 +17,13 @@
  */
 package se.vgregion.kivtools.search.presentation;
 
-
 import org.easymock.EasyMock;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import se.vgregion.kivtools.search.exceptions.KivNoDataFoundException;
 import se.vgregion.kivtools.search.presentation.forms.UnitSearchSimpleForm;
@@ -32,47 +33,55 @@ import se.vgregion.kivtools.search.svc.domain.Unit;
 
 public class SearchUnitFlowSupportBeanTest {
 
-    @Before
-    public void setUp() throws Exception {
-    }
+	@Before
+	public void setUp() throws Exception {
+	}
 
-    @After
-    public void tearDown() throws Exception {
-    }
+	@After
+	public void tearDown() throws Exception {
+	}
 
-    @Test
-    public void testDoSearch() throws Exception {
-        SearchService ssMock = EasyMock.createNiceMock(SearchService.class);
-        SearchUnitFlowSupportBean bean = new SearchUnitFlowSupportBean();
-        bean.setSearchService(ssMock);
-        bean.setMaxSearchResult(200);
-        bean.setPageSize(25);
-        
-        
-        // doSearch corresponds to 
-        // expect one search - mock rercord mode
-        Unit unit = new Unit();
-        unit.setName("my unit");
-        SikSearchResultList<Unit> unitMockList = new SikSearchResultList<Unit>();
-        unitMockList.add(unit);
-        EasyMock.expect(ssMock.searchUnits(unit, 200)).andReturn(unitMockList);
-        EasyMock.replay(ssMock);
-        
-        // run some test
-        SikSearchResultList<Unit> ul = null;
-        UnitSearchSimpleForm theForm = new UnitSearchSimpleForm();
-        theForm.setUnitName("my unit");
-        try {
-           ul = bean.doSearch(theForm);
-           Assert.fail("A KivNoDataFoundException should have been thrown");
-        } catch (KivNoDataFoundException e) {
-            // that is ok
-        }
-        
-        // verify
-        Assert.assertNull(ul);
-        Assert.assertEquals(1, unitMockList.size());
-        //EasyMock.verify(ssMock);
-    }
+	@Test
+	public void testDoSearch() throws Exception {
+		SearchService ssMock = EasyMock.createNiceMock(SearchService.class);
+		SearchUnitFlowSupportBean bean = new SearchUnitFlowSupportBean();
+		bean.setSearchService(ssMock);
+		bean.setMaxSearchResult(200);
+		bean.setPageSize(25);
 
+		// doSearch corresponds to
+		// expect one search - mock record mode
+		Unit unit = new Unit();
+		unit.setName("my unit");
+		SikSearchResultList<Unit> unitMockList = new SikSearchResultList<Unit>();
+		unitMockList.add(unit);
+		EasyMock.expect(ssMock.searchUnits(unit, 200)).andReturn(unitMockList);
+		EasyMock.replay(ssMock);
+
+		// run some test
+		SikSearchResultList<Unit> ul = null;
+		UnitSearchSimpleForm theForm = new UnitSearchSimpleForm();
+		theForm.setUnitName("my unit");
+		try {
+			ul = bean.doSearch(theForm);
+			Assert.fail("A KivNoDataFoundException should have been thrown");
+		} catch (KivNoDataFoundException e) {
+			// that is ok
+		}
+
+		// verify
+		Assert.assertNull(ul);
+		Assert.assertEquals(1, unitMockList.size());
+		// EasyMock.verify(ssMock);
+	}
+	
+	// This is an integration test against a working LDAP server
+	@Test
+	@Ignore
+	public void testGetSubUnits(){
+		ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext("services-config.xml");
+		SearchUnitFlowSupportBean searchUnitFlowSupportBean = (SearchUnitFlowSupportBean) applicationContext.getBean("Search.SearchUnitFlowSupportBean");
+		SikSearchResultList<Unit> subUnits = searchUnitFlowSupportBean.getSubUnits("SE2321000131-E000000000117");
+		System.out.println("");
+	}
 }
