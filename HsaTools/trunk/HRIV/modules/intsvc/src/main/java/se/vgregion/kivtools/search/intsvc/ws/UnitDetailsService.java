b4a981f622ab2303562224364731cda41ca18a8d
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-05-14 15:20:44 +0000

    Changed WS to use spring AbstractMarshallingPayloadEndpoint

--- HsaTools/trunk/HRIV/modules/intsvc/src/main/java/se/vgregion/kivtools/search/intsvc/ws/UnitDetailsService.java
+++ HsaTools/trunk/HRIV/modules/intsvc/src/main/java/se/vgregion/kivtools/search/intsvc/ws/UnitDetailsService.java
@@ -1,5 +1,5 @@
 package se.vgregion.kivtools.search.intsvc.ws;
 
-public interface UnitDetailsService {
-	org.w3c.dom.Element getUnitDetails(String hsaIdentity);
+public interface UnitDetailsService <T>{
+	T getUnitDetails(String hsaIdentity);
 }
