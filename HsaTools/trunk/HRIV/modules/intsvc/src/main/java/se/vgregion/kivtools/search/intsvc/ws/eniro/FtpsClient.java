a9b90a42d265655f5002bf02b799aa513a55522d
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-06-06 10:04:36 +0000

    Extracted ftp functionality from InformationPusher and create new Interface Ftpclient to use instead. This enable to use different type of ftp clients / David & Jonas

--- HsaTools/trunk/HRIV/modules/intsvc/src/main/java/se/vgregion/kivtools/search/intsvc/ws/eniro/FtpsClient.java
+++ HsaTools/trunk/HRIV/modules/intsvc/src/main/java/se/vgregion/kivtools/search/intsvc/ws/eniro/FtpsClient.java
@@ -1,12 +1,12 @@
 package se.vgregion.kivtools.search.intsvc.ws.eniro;
 
 import java.io.File;
+import java.io.FileInputStream;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.commons.net.ftp.FTPSClient;
 import org.apache.commons.net.ftp.FTPSCommand;
-
 
 public class FtpsClient implements FtpClient {
 
@@ -16,7 +16,7 @@ public class FtpsClient implements FtpClient {
 	private int port;
 	private String username;
 	private String password;
-	
+	private String ftpDestinationFileName;
 
 	public void setFtpclient(FTPSClient ftpsclient) {
 		this.ftpsclient = ftpsclient;
@@ -24,19 +24,21 @@ public class FtpsClient implements FtpClient {
 
 	public boolean sendFile(File file) {
 		try {
-			
+			FileInputStream fileInputStream = new FileInputStream(file);
 			ftpsclient.connect(hostname, port);
 			ftpsclient.login(username, password);
-		    //ftpsclient.appendFile(remote, local)
+			boolean storedSuccess = ftpsclient.storeFile(ftpDestinationFileName, fileInputStream);
+			fileInputStream.close();
 			// Logout from the FTP Server and disconnect
 			ftpsclient.logout();
 			ftpsclient.disconnect();
-
+			return storedSuccess;
 		} catch (Exception e) {
 			logger.error("Error in SftpClient", e);
 		}
 		return false;
 	}
+
 	public static void main(String[] args) {
 		System.out.println(FTPSCommand.getCommand(FTPSCommand.PROT));
 	}
