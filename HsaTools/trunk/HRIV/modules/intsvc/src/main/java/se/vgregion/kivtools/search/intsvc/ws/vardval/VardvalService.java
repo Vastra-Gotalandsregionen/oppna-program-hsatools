268b25972540f39924b6ca57968c378fac1253fa
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-06-16 14:51:40 +0000

    VardvalService implementation / David & Jonas

---
+++ HsaTools/trunk/HRIV/modules/intsvc/src/main/java/se/vgregion/kivtools/search/intsvc/ws/vardval/VardvalService.java
@@ -0,0 +1,19 @@
+package se.vgregion.kivtools.search.intsvc.ws.vardval;
+
+public interface VardvalService {
+	
+	/**
+	 * 
+	 * @param sn - Social Security Number
+	 * @return - Javabean
+	 */
+	VardvalInfo getVardval(String ssn);
+	
+	/**
+	 * 
+	 * @param sn - Social Security Number
+	 * @param hsaId - HsaIdentity
+	 * @param signature - Personal signature
+	 */
+	VardvalInfo setVardval(String ssn, String hsaId, byte[] signature);
+}
