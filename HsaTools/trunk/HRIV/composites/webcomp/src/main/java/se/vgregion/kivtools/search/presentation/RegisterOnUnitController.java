a7e38edbed51866385b2e69c19d29800de379695
jonas.liljenfeldt <jonas.liljenfeldt@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-06-24 11:56:25 +0000

    Initial, and not complete at all, support for vårdval. /David and Jonas

--- HsaTools/trunk/HRIV/composites/webcomp/src/main/java/se/vgregion/kivtools/search/presentation/RegisterOnUnitController.java
+++ HsaTools/trunk/HRIV/composites/webcomp/src/main/java/se/vgregion/kivtools/search/presentation/RegisterOnUnitController.java
@@ -1,80 +1,83 @@
 package se.vgregion.kivtools.search.presentation;
 
-
 import java.io.Serializable;
 import java.util.Map;
 
 import org.springframework.webflow.context.ExternalContext;
-import org.springframework.webflow.context.servlet.ServletExternalContext;
 import org.springframework.webflow.executor.jsf.JsfExternalContext;
 
+import se.vgregion.kivtools.search.presentation.forms.RegistrationConfirmationForm;
+import se.vgregion.kivtools.search.svc.SearchService;
+import se.vgregion.kivtools.search.svc.domain.Unit;
+import se.vgregion.kivtools.search.svc.ws.vardval.VardvalInfo;
 import se.vgregion.kivtools.search.svc.ws.vardval.VardvalService;
 
 public class RegisterOnUnitController implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	VardvalService vardValService;
-	
+	SearchService searchService;
+
+	public void setSearchService(SearchService searchService) {
+		this.searchService = searchService;
+	}
+
 	public void setVardValService(VardvalService vardValService) {
 		this.vardValService = vardValService;
 	}
 
-	public RegistrationSupportBean getUnitRegistrationInformation(ExternalContext externalContext) {
-		
-		JsfExternalContext jsfExternalContext = (JsfExternalContext) externalContext;
-		
-		// Put ssn in sessionscope object
-		////JsfExternalContext externalContext = (JsfExternalContext) requestContext.getExternalContext();
-		//Cookie[] cookies = externalContext.getRequest().getCookies();
-		/*
-		String ltpaCookieToken = null;
-		String ltpaCookieName = "LtpaToken";
+	public VardvalInfo getUnitRegistrationInformation(ExternalContext externalContext, String selectedUnitId) throws Exception {
 
-		// Get LTPA token
-		for (int i = 0; i < cookies.length; i++) {
-			if (cookies[i].getName().equals(ltpaCookieName)) {
-				ltpaCookieToken = cookies[i].getValue();
-			}
-		}
-		
-		// LTPA token is base64 encoded
-		byte[] ltpaTokenKey = Base64.decode(ltpaCookieToken);
-		*/
-		
+		JsfExternalContext jsfExternalContext = (JsfExternalContext) externalContext;
+
 		// WAS returns null. Maybe because the url is not protected in web.xml?
-		////Principal remoteUser = externalContext.getUserPrincipal();
-		//String name = remoteUser.getName();
-		
-		// Find ssn as http header (put there by WebSphere)
-		//Map<String, String> requestHeaderMap = externalContext.getFacesContext().getExternalContext().getRequestHeaderMap();
-		
+		// Principal remoteUser = externalContext.getUserPrincipal();
+
 		Map<String, String> requestHeaderMap = jsfExternalContext.getFacesContext().getExternalContext().getRequestHeaderMap();
-		
+
 		String ssn = requestHeaderMap.get("iv-user");
-		//String ldapPath = requestHeaderMap.get("iv-user-1");
-		System.out.println("ssn: " + ssn);
-		//System.out.println("ldapPath: " + ldapPath);
-		//System.out.print("name: " + name);
-		
-		//VardvalInfo vardval = vardValService.getVardval(ssn);
-		
-		// Set the results in "request scope"
-		//requestContext.getRequestScope().put("vardval", vardval);
-		//requestContext.getFlowScope().put("ssn", ssn);
-		//requestContext.getFlowScope().put("ldapPath", ldapPath);
-		//requestContext.getRequestScope().put("name", name);
-		
-		//Event event = new Event(this, "success");
-		//return event;
-		
-		RegistrationSupportBean rsb = new RegistrationSupportBean();
-		rsb.setSsn(ssn);
-		return rsb;
+		// String ldapPath = requestHeaderMap.get("iv-user-1");
+
+		// FIXME Remove this line
+		ssn = "194509259257";
+
+		// Request information about the listing from Vårdvals system's
+		// webservice
+		VardvalInfo vardvalInfo = new VardvalInfo();
+		try {
+			vardvalInfo = vardValService.getVardval(ssn);
+
+			// Lookup unit names in order to show real names instead of hsa ids
+			// TODO Exception handling
+			Unit selectedUnit = searchService.getUnitByHsaId(selectedUnitId);
+			if (selectedUnit != null) {
+				vardvalInfo.setSelectedUnitName(selectedUnit.getName());
+			}
+			vardvalInfo.setSelectedUnitId(selectedUnitId);
+			Unit currentUnit = searchService.getUnitByHsaId(vardvalInfo.getCurrentHsaId());
+			Unit upcomingUnit = searchService.getUnitByHsaId(vardvalInfo.getUpcomingHsaId());
+
+			// Set values in DTO
+			if (currentUnit != null) {
+				vardvalInfo.setCurrentUnitName(currentUnit.getName());
+			}
+			if (upcomingUnit != null) {
+				vardvalInfo.setUpcomingUnitName(upcomingUnit.getName());
+			}
+			vardvalInfo.setVardvalInfo(vardvalInfo);
+		} catch (Exception e) {
+			// FIXME Proper exception handling. Show information page if we
+			// could not find unit with given hsa id
+		}
+
+		vardvalInfo.setSsn(ssn);
+		return vardvalInfo;
 	}
 
-	//public void commitRegistrationOnUnit() {
-		// we need String socialSecurityNumber, String hsaIdentity) {
-//		return success();
-	//}
+	public String commitRegistrationOnUnit(VardvalInfo vardvalInfo) {
+		// signature should be byte[]
+		vardValService.setVardval(vardvalInfo.getSsn(), vardvalInfo.getSelectedUnitId(), "".getBytes());//vardvalInfo.getSignature().getBytes());
+		return "success";
+	}
 
 }
--- HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalInfo.java
+++ HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalInfo.java
@@ -1,13 +1,88 @@
 package se.vgregion.kivtools.search.svc.ws.vardval;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class VardvalInfo {
+public class VardvalInfo implements Serializable {
 
+	private static final long serialVersionUID = 1L;
+	
 	private String currentHsaId;
 	private Date currentValidFromDate;
 	private String upcomingHsaId;
 	private Date upcomingValidFromDate;
+	private VardvalInfo vardvalInfo;
+	private String ssn;
+	private String name;
+	private String selectedUnitId;
+	private String upcomingUnitName;
+	private String currentUnitName;
+	private String selectedUnitName;
+	private String signature;
+
+	public String getSignature() {
+		return signature;
+	}
+
+	public void setSignature(String signature) {
+		this.signature = signature;
+	}
+
+	public String getSelectedUnitId() {
+		return selectedUnitId;
+	}
+
+	public void setSelectedUnitId(String selectedUnitId) {
+		this.selectedUnitId = selectedUnitId;
+	}
+
+	public String getSelectedUnitName() {
+		return selectedUnitName;
+	}
+
+	public void setSelectedUnitName(String selectedUnitName) {
+		this.selectedUnitName = selectedUnitName;
+	}
+
+	public String getUpcomingUnitName() {
+		return upcomingUnitName;
+	}
+
+	public void setUpcomingUnitName(String upcomingUnitName) {
+		this.upcomingUnitName = upcomingUnitName;
+	}
+
+	public String getCurrentUnitName() {
+		return currentUnitName;
+	}
+
+	public void setCurrentUnitName(String currentUnitName) {
+		this.currentUnitName = currentUnitName;
+	}
+
+	public VardvalInfo getVardvalInfo() {
+		return vardvalInfo;
+	}
+
+	public void setVardvalInfo(VardvalInfo vardvalInfo) {
+		this.vardvalInfo = vardvalInfo;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
 
 	public String getCurrentHsaId() {
 		return currentHsaId;
