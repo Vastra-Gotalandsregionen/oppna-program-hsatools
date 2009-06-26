53ece0fc24a3c3854471da6abb810b7f417815fa
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-06-26 07:23:02 +0000

    Refactoring of RegistOnUnitController / David & Jonas

--- HsaTools/trunk/HRIV/composites/webcomp/src/test/java/se/vgregion/kivtools/search/presentation/RegisterOnUnitControllerTest.java
+++ HsaTools/trunk/HRIV/composites/webcomp/src/test/java/se/vgregion/kivtools/search/presentation/RegisterOnUnitControllerTest.java
@@ -1,51 +1,93 @@
 package se.vgregion.kivtools.search.presentation;
 
+import static org.easymock.EasyMock.expect;
+import static org.easymock.classextension.EasyMock.createMock;
+import static org.easymock.classextension.EasyMock.replay;
 import static org.junit.Assert.assertEquals;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.easymock.classextension.EasyMock;
+import javax.faces.context.FacesContext;
+
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.springframework.webflow.context.servlet.ServletExternalContext;
-import org.springframework.webflow.execution.Event;
-import org.springframework.webflow.execution.RequestContext;
+import org.springframework.webflow.executor.jsf.JsfExternalContext;
+
+import se.vgregion.kivtools.search.svc.SearchService;
+import se.vgregion.kivtools.search.svc.domain.Unit;
+import se.vgregion.kivtools.search.svc.ws.vardval.VardvalInfo;
+import se.vgregion.kivtools.search.svc.ws.vardval.VardvalService;
 
 public class RegisterOnUnitControllerTest {
 
 	RegisterOnUnitController registerOnUnitController;
+	private final String SELECTED_UNIT_ID = "SE2321000131-E000000001302";
+	private final String CURRENT_UNIT_ID = "SE2321000131-E000000001303";
+	private final String UPCOMING_UNIT_ID = "SE2321000131-E000000001304";
+	private final String SELECTED_UNIT_NAME = "Selected unit name";
+	private final String CURRENT_UNIT_NAME = "Current unit name";
+	private final String UPCOMING_UNIT_NAME = "Upcoming unit name";
+
+	private final String SSN = "197702200101";
+	private final Unit selectedUnit = new Unit();
+	private final Unit currentUnit = new Unit();
+	private final Unit upcomingUnit = new Unit();
 
 	@Before
-	public void setup() {
+	public void setup() throws Exception {
 		registerOnUnitController = new RegisterOnUnitController();
-
+		registerOnUnitController.setVardValService(generateVardvalServiceMock());
+		registerOnUnitController.setSearchService(generateSearchServiceMock());
+		selectedUnit.setName(SELECTED_UNIT_NAME);
+		currentUnit.setName(CURRENT_UNIT_NAME);
+		upcomingUnit.setName(UPCOMING_UNIT_NAME);
 	}
 
 	@Test
-	@Ignore
-	public void testExtractSessionInformation() {
-		
-		RequestContext mockRequestContext = EasyMock.createMock(RequestContext.class);
-		HttpServletRequest mockHttpServletRequest = EasyMock.createMock(HttpServletRequest.class);	
-		
-		Cookie[] cookies = {new Cookie("LtpaToken","base64encodedName" )};
-		
-		ServletExternalContext mockServletExternalContext = EasyMock.createMock(ServletExternalContext.class);
-		
-		// 1 Get servletexternalcontext
-		EasyMock.expect(mockRequestContext.getExternalContext()).andReturn(mockServletExternalContext);
-		
-		// 2 Get httpservletrequest
-		EasyMock.expect(mockServletExternalContext.getRequest()).andReturn(mockHttpServletRequest);
+	public void testExtractSessionInformation() throws Exception {
 
-		// 3 Get cookies
-		EasyMock.expect(mockHttpServletRequest.getCookies()).andReturn(cookies);
-		
-		EasyMock.replay(mockRequestContext, mockServletExternalContext, mockHttpServletRequest);
-		
-		//Event event = registerOnUnitController.getUnitRegistrationInformation(mockRequestContext);
-		//assertEquals("success", event.getId());
+		VardvalInfo vardvalInfo = registerOnUnitController.getUnitRegistrationInformation(generateContextMock(), SELECTED_UNIT_ID);
+		// Check selected unit info
+		assertEquals(SELECTED_UNIT_NAME, vardvalInfo.getSelectedUnitName());
+		assertEquals(SELECTED_UNIT_ID, vardvalInfo.getSelectedUnitId());
+		// Check current unit info
+		assertEquals(CURRENT_UNIT_NAME, vardvalInfo.getCurrentUnitName());
+		assertEquals(CURRENT_UNIT_ID, vardvalInfo.getCurrentHsaId());
+		// Check upcoming unit info
+		assertEquals(UPCOMING_UNIT_NAME, vardvalInfo.getUpcomingUnitName());
+		assertEquals(UPCOMING_UNIT_ID, vardvalInfo.getUpcomingHsaId());
+	}
+
+	private JsfExternalContext generateContextMock() {
+		JsfExternalContext mockJsfExternalContext = createMock(JsfExternalContext.class);
+		FacesContext mockFacesContext = createMock(FacesContext.class);
+		javax.faces.context.ExternalContext mockExternalContext = createMock(javax.faces.context.ExternalContext.class);
+		expect(mockJsfExternalContext.getFacesContext()).andReturn(mockFacesContext);
+		expect(mockFacesContext.getExternalContext()).andReturn(mockExternalContext);
+		Map<String, String> requestHeaderMap = new HashMap<String, String>();
+		requestHeaderMap.put("iv-user", SSN);
+		expect(mockExternalContext.getRequestHeaderMap()).andReturn(requestHeaderMap);
+		replay(mockJsfExternalContext, mockFacesContext, mockExternalContext);
+		return mockJsfExternalContext;
+	}
+
+	private SearchService generateSearchServiceMock() throws Exception {
+		SearchService mockSearchService = createMock(SearchService.class);
+		expect(mockSearchService.getUnitByHsaId(SELECTED_UNIT_ID)).andReturn(selectedUnit);
+		expect(mockSearchService.getUnitByHsaId(CURRENT_UNIT_ID)).andReturn(currentUnit);
+		expect(mockSearchService.getUnitByHsaId(UPCOMING_UNIT_ID)).andReturn(upcomingUnit);
+		replay(mockSearchService);
+		return mockSearchService;
+	}
+
+	private VardvalService generateVardvalServiceMock() {
+		VardvalInfo vardvalInfo = new VardvalInfo();
+		vardvalInfo.setCurrentHsaId(CURRENT_UNIT_ID);
+		vardvalInfo.setUpcomingHsaId(UPCOMING_UNIT_ID);
+		VardvalService mockVardvalService = createMock(VardvalService.class);
+		expect(mockVardvalService.getVardval(SSN)).andReturn(vardvalInfo);
+		replay(mockVardvalService);
+		return mockVardvalService;
 	}
 }
