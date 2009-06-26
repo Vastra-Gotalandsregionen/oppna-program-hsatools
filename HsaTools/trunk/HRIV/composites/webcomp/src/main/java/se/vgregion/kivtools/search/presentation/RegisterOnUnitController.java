1950fca30685a3a6c980b91cfd8b1f17c6a4c72c
jonas.liljenfeldt <jonas.liljenfeldt@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-06-26 13:33:57 +0000

    Initial, and not complete at all, support for vårdval. /David and Jonas

--- HsaTools/trunk/HRIV/composites/webcomp/src/main/java/se/vgregion/kivtools/search/presentation/RegisterOnUnitController.java
+++ HsaTools/trunk/HRIV/composites/webcomp/src/main/java/se/vgregion/kivtools/search/presentation/RegisterOnUnitController.java
@@ -1,13 +1,23 @@
 package se.vgregion.kivtools.search.presentation;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Enumeration;
 import java.util.Map;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.webflow.context.ExternalContext;
+import org.springframework.webflow.executor.jsf.FlowExecutionKeyStateHolder;
 import org.springframework.webflow.executor.jsf.JsfExternalContext;
+import org.springframework.webflow.executor.mvc.FlowController;
 
 import se.vgregion.kivtools.search.svc.SearchService;
 import se.vgregion.kivtools.search.svc.domain.Unit;
+import se.vgregion.kivtools.search.svc.ws.signicat.signature.SignatureEndpointImpl;
+import se.vgregion.kivtools.search.svc.ws.signicat.signature.SignatureEndpointImplService;
 import se.vgregion.kivtools.search.svc.ws.vardval.VardvalInfo;
 import se.vgregion.kivtools.search.svc.ws.vardval.VardvalService;
 
@@ -16,6 +26,10 @@ public class RegisterOnUnitController implements Serializable {
 	private static final long serialVersionUID = 1L;
 	VardvalService vardValService;
 	SearchService searchService;
+	String signatureserviceUrl = "https://test.signicat.com/signatureservice/services/signatureservice";
+	String serviceUrl = "https://test.signicat.com/std/method/shared?method=sign&profile=default";
+	SignatureEndpointImplService signatureEndpointImplService = new SignatureEndpointImplService();
+	SignatureEndpointImpl signatureservice = signatureEndpointImplService.getSignatureservice();
 
 	public void setSearchService(SearchService searchService) {
 		this.searchService = searchService;
@@ -38,13 +52,13 @@ public class RegisterOnUnitController implements Serializable {
 		// String ldapPath = requestHeaderMap.get("iv-user-1");
 
 		// FIXME Remove this line
-		//ssn = "194509259257";
+		ssn = "194509259257";
 
 		// Request information about the listing from Vårdvals system's
 		// webservice
 		VardvalInfo vardvalInfo = new VardvalInfo();
 		try {
-			vardvalInfo = vardValService.getVardval(ssn);
+			////vardvalInfo = vardValService.getVardval(ssn);
 
 			// Lookup unit names in order to show real names instead of hsa ids
 			// TODO Exception handling
@@ -72,9 +86,86 @@ public class RegisterOnUnitController implements Serializable {
 		return vardvalInfo;
 	}
 
-	public String commitRegistrationOnUnit(VardvalInfo vardvalInfo) {
+	/**
+	 * 
+	 * @param vardvalInfo
+	 * @param externalContext
+	 * @return redirectUrl The url to redirect to
+	 * @throws UnsupportedEncodingException
+	 */
+	public String preCommitRegistrationOnUnit(VardvalInfo vardvalInfo, ExternalContext externalContext) {
+		String redirectUrl = "";
+		try {
+
+			String registrationData = "197407185656_" + "SE2321000131-E000000006727" + "_20090626094900";
+			byte[] base64encoded = Base64.encodeBase64(registrationData.getBytes("UTF-8"));
+			String mimeType = "text/plain";
+			String documentDescription = "Signature for Vardval registration";
+			String artifact = signatureservice.registerDocument(new String(base64encoded), mimeType, documentDescription);
+			System.out.println("Artifact recieved: " + artifact);
+			// Save artifact
+			externalContext.getSessionMap().put("artifact", artifact);
+
+			// Construct redirect url
+			JsfExternalContext jsfExternalContext = (JsfExternalContext) externalContext;
+			HttpServletRequest httpServletRequest = (HttpServletRequest) jsfExternalContext.getFacesContext().getExternalContext().getRequest();
+			String urlToThisPage = httpServletRequest.getRequestURL().toString();
+			String eventId = "Action.postChangeRegistration";
+			
+			
+			// TODO Extract flowExecutionKey in a nicer way
+			Enumeration<String> parameterNames = httpServletRequest.getParameterNames();
+			String flowExecutionKey = "";
+			while (parameterNames.hasMoreElements()) {
+				String paramName = parameterNames.nextElement();
+				if (paramName.indexOf("_flowExecutionKey") > -1) {
+					flowExecutionKey = httpServletRequest.getParameter(paramName); 
+				}
+			}
+			
+			String targetUrl = urlToThisPage.substring(0, urlToThisPage.lastIndexOf("/") + 1) + "confirmationRegistrationChanges.jsf?_flowId=HRIV.registrationOnUnit-flow&_eventId=" + eventId
+					+ "&_flowExecutionKey=" + flowExecutionKey + "&redirectedFromSignatureService=true";
+
+			// Redirect user to test.signicat.com
+			System.out.println("Redirects the user to " + targetUrl);
+			redirectUrl = serviceUrl + "&documentArtifact=" + artifact + "&target=" + URLEncoder.encode(targetUrl, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			redirectUrl = "FELSIDA";
+			// FIXME
+		}
+		return redirectUrl;
+	}
+
+	public String postCommitRegistrationOnUnit(VardvalInfo vardvalInfo, ExternalContext externalContext) {
+
+		String errorMessage;
+		byte[] samlAssertionBytes = new byte[0];
+		
+		String artifact = (String) externalContext.getSessionMap().get("artifact");
+		String samlAssertion = signatureservice.retrieveSaml(artifact);
+		System.out.println("Saml Assertion recieved");
+		if (samlAssertion == null || samlAssertion.length() == 0) {
+			errorMessage = "SAML assertion was empty";
+		}
+		// if (errorMessage == null) {
+		// samlAssertionBytes =
+		// Base64.decodeBase64(samlAssertion.getBytes("utf-8"));
+		System.out.println("Saml Assertion decoded");
+		// }
+
 		// signature should be byte[]
-		vardValService.setVardval(vardvalInfo.getSsn(), vardvalInfo.getSelectedUnitId(), "".getBytes());//vardvalInfo.getSignature().getBytes());
+		// vardValService.setVardval(vardvalInfo.getSsn(),
+		// vardvalInfo.getSelectedUnitId(), "".getBytes());// vardvalInfo
+		// .
+		// getSignature
+		// (
+		// )
+		// .
+		// getBytes
+		// (
+		// )
+		// )
+		// ;
 		return "success";
 	}
 
