1f21267b78a4f0a11f3a51631549b8d92d8e4bea
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-07-08 07:23:17 +0000

    Refactoring of VardvalServiceImpl

---
+++ HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalServiceFactory.java
@@ -0,0 +1,12 @@
+package se.vgregion.kivtools.search.svc.ws.vardval;
+
+import se.vgregion.kivtools.search.svc.ws.domain.vardval.IVårdvalService;
+import se.vgregion.kivtools.search.svc.ws.domain.vardval.VårdvalService;
+
+public class VardvalServiceFactory {
+	
+	public static IVårdvalService getIVardvalservice(){
+		return new VårdvalService().getBasicHttpBindingIVårdvalService();
+	}
+
+}
--- HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalServiceTest.java
+++ HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalServiceTest.java
@@ -22,12 +22,10 @@ import se.vgregion.kivtools.search.svc.ws.domain.vardval.ObjectFactory;
 import se.vgregion.kivtools.search.svc.ws.domain.vardval.SetVårdvalRequest;
 import se.vgregion.kivtools.search.svc.ws.domain.vardval.SetVårdvalResponse;
 import se.vgregion.kivtools.search.svc.ws.domain.vardval.VårdvalEntry;
-import se.vgregion.kivtools.search.svc.ws.domain.vardval.VårdvalService;
 
 import com.sun.org.apache.xerces.internal.jaxp.datatype.XMLGregorianCalendarImpl;
 
 public class VardvalServiceTest {
-
 
 	private static Date upcomingValidFromDate;
 	private static Date currentValidFromDate;
@@ -35,107 +33,103 @@ public class VardvalServiceTest {
 	private static final String upcomingHsaId = "116";
 	private static final String ssn = "194509259257";
 	ObjectFactory objectFactory = new ObjectFactory();
-	VårdvalService mockVardvalService;
 
 	@BeforeClass
-	public static void setupSimpleDateFormat() throws ParseException{
+	public static void setupSimpleDateFormat() throws ParseException {
 		upcomingValidFromDate = new SimpleDateFormat("yyyy-MM-dd").parse("2009-06-17");
 		currentValidFromDate = new SimpleDateFormat("yyyy-MM-dd").parse("2002-12-10");
 	}
-	
-@Test
-	/**
-	 * Test getting Vardval information for a person.
+
+	@Test
+	/*
+	 * * Test getting Vardval information for a person.
 	 */
-public void testGetVardvalMethod() throws ParseException {
-		mockVardvalService = EasyMock.createMock(VårdvalService.class);
+	public void testGetVardvalMethod() throws Exception {
 		IVårdvalService mockService = EasyMock.createMock(IVårdvalService.class);
 		GetVårdvalResponse getVardvalResponse = new GetVårdvalResponse();
 		generateResponse(getVardvalResponse);
 
-		EasyMock.expect(mockVardvalService.getBasicHttpBindingIVårdvalService()).andReturn(mockService);
 		EasyMock.expect(mockService.getVårdVal(EasyMock.isA(GetVårdvalRequest.class))).andReturn(getVardvalResponse);
-		EasyMock.replay(mockVardvalService, mockService);
+		EasyMock.replay(mockService);
 
 		VardvalService vardvalService = new VardvalServiceImpl();
-		((VardvalServiceImpl) vardvalService).setVardvalService(mockVardvalService);
+
+		((VardvalServiceImpl) vardvalService).setService(mockService);
 		VardvalInfo vardvalInfo = vardvalService.getVardval(ssn);
 
 		assertEquals(currentHsaId, vardvalInfo.getCurrentHsaId());
 		assertEquals(currentValidFromDate, vardvalInfo.getCurrentValidFromDate());
 		assertEquals(upcomingHsaId, vardvalInfo.getUpcomingHsaId());
 		assertEquals(upcomingValidFromDate, vardvalInfo.getUpcomingValidFromDate());
-}
+	}
 
 	@Test
-	/**
-	 * Test assigning new listing and check retrieved Vardval information. 
+	/*
+	 * * Test assigning new listing and check retrieved Vardval information.
 	 */
 	public void testSetVardvalMethod() throws IVårdvalServiceSetVårdValVårdvalServiceErrorFaultFaultMessage {
-		mockVardvalService = EasyMock.createMock(VårdvalService.class);
 		IVårdvalService mockService = EasyMock.createMock(IVårdvalService.class);
 		SetVårdvalResponse setVardvalResponse = new SetVårdvalResponse();
 		generateResponse(setVardvalResponse);
 
-		EasyMock.expect(mockVardvalService.getBasicHttpBindingIVårdvalService()).andReturn(mockService);
 		EasyMock.expect(mockService.setVårdVal(EasyMock.isA(SetVårdvalRequest.class))).andReturn(setVardvalResponse);
-		EasyMock.replay(mockVardvalService, mockService);
+		EasyMock.replay(mockService);
 
 		VardvalService vardvalService = new VardvalServiceImpl();
-		((VardvalServiceImpl) vardvalService).setVardvalService(mockVardvalService);
+		((VardvalServiceImpl) vardvalService).setService(mockService);
 		VardvalInfo vardvalInfo = vardvalService.setVardval(ssn, upcomingHsaId, new byte[] {});
 
 		assertEquals(upcomingHsaId, vardvalInfo.getUpcomingHsaId());
 		assertEquals(currentValidFromDate, vardvalInfo.getCurrentValidFromDate());
 		assertEquals(upcomingValidFromDate, vardvalInfo.getUpcomingValidFromDate());
 	}
-	
+
 	private void generateResponse(GetVårdvalResponse getVardvalResponse) {
 		VårdvalEntry currentEntry = new VårdvalEntry();
 		VårdvalEntry upcomingEntry = new VårdvalEntry();
 		// Set current and upcoming dates for vårdval entries
 		GregorianCalendar calendar = (GregorianCalendar) GregorianCalendar.getInstance();
 		calendar.setTime(currentValidFromDate);
-		XMLGregorianCalendar currentGregorianCalendar = new XMLGregorianCalendarImpl(calendar);		
+		XMLGregorianCalendar currentGregorianCalendar = new XMLGregorianCalendarImpl(calendar);
 		calendar.setTime(upcomingValidFromDate);
-		XMLGregorianCalendar upcomingGregorianCalendar = new XMLGregorianCalendarImpl(calendar);		
+		XMLGregorianCalendar upcomingGregorianCalendar = new XMLGregorianCalendarImpl(calendar);
 		currentEntry.setGiltigFrån(currentGregorianCalendar);
 		upcomingEntry.setGiltigFrån(upcomingGregorianCalendar);
-		
+
 		// Set hsaId:s
 		currentEntry.setVårdcentralHsaId(currentHsaId);
 		upcomingEntry.setVårdcentralHsaId(upcomingHsaId);
-		
+
 		JAXBElement<VårdvalEntry> currentJaxbElement = objectFactory.createVårdvalEntry(currentEntry);
-		JAXBElement<VårdvalEntry> upcomingJaxbElement =objectFactory.createVårdvalEntry(upcomingEntry);
-		
+		JAXBElement<VårdvalEntry> upcomingJaxbElement = objectFactory.createVårdvalEntry(upcomingEntry);
+
 		getVardvalResponse.setAktivtVårdval(currentJaxbElement);
 		getVardvalResponse.setKommandeVårdval(upcomingJaxbElement);
-	
+
 	}
-	
+
 	private void generateResponse(SetVårdvalResponse setVardvalResponse) {
 		VårdvalEntry currentEntry = new VårdvalEntry();
 		VårdvalEntry upcomingEntry = new VårdvalEntry();
 		// Set current and upcoming dates for vårdval entries
 		GregorianCalendar calendar = (GregorianCalendar) GregorianCalendar.getInstance();
 		calendar.setTime(currentValidFromDate);
-		XMLGregorianCalendar currentGregorianCalendar = new XMLGregorianCalendarImpl(calendar);		
+		XMLGregorianCalendar currentGregorianCalendar = new XMLGregorianCalendarImpl(calendar);
 		calendar.setTime(upcomingValidFromDate);
-		XMLGregorianCalendar upcomingGregorianCalendar = new XMLGregorianCalendarImpl(calendar);		
+		XMLGregorianCalendar upcomingGregorianCalendar = new XMLGregorianCalendarImpl(calendar);
 		currentEntry.setGiltigFrån(currentGregorianCalendar);
 		upcomingEntry.setGiltigFrån(upcomingGregorianCalendar);
-		
+
 		// Set hsaId:s
 		currentEntry.setVårdcentralHsaId(currentHsaId);
 		upcomingEntry.setVårdcentralHsaId(upcomingHsaId);
-		
+
 		JAXBElement<VårdvalEntry> currentJaxbElement = objectFactory.createVårdvalEntry(currentEntry);
-		JAXBElement<VårdvalEntry> upcomingJaxbElement =objectFactory.createVårdvalEntry(upcomingEntry);
-		
+		JAXBElement<VårdvalEntry> upcomingJaxbElement = objectFactory.createVårdvalEntry(upcomingEntry);
+
 		setVardvalResponse.setAktivtVårdval(currentJaxbElement);
 		setVardvalResponse.setKommandeVårdval(upcomingJaxbElement);
-	
+
 	}
 
 }
