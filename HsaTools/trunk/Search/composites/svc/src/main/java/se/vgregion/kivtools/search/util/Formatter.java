6f3f97efa509e781f062935dfdf4cd9ddeaf0edb
argoyle <argoyle@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-07-08 11:43:01 +0000

    Cleanup of Unit. /Joakim

--- HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/util/Formatter.java
+++ HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/util/Formatter.java
@@ -33,143 +33,149 @@ import org.apache.commons.logging.LogFactory;
  * 
  */
 public class Formatter {
-	private static String CLASS_NAME = Formatter.class.getName();
-	static Log logger = LogFactory.getLog(CLASS_NAME);
+  private static String CLASS_NAME = Formatter.class.getName();
+  static Log logger = LogFactory.getLog(CLASS_NAME);
 
-	/**
-	 * Replace all occurances of stringToReplace in string originalString with
-	 * replacingString.
-	 * 
-	 * E.g. originalString = "hello'all'" another example: =
-	 * "2005 Gold 2004 Gold" stringToReplace = "\'" = "Gold" replacingString =
-	 * "''" = "Guld"
-	 * 
-	 * returns "hello''all''" = "2005 Guld 2004 Guld"
-	 */
-	public static String replaceStringInString(String originalString, String stringToReplace, String replacingString) throws Exception {
-		String methodName = "replaceStringInString(originalString=" + originalString + ", stringToReplace=" + stringToReplace + ", replacingString=" + replacingString + ")";
-		String finalString = "";
-		if (originalString == null) {
-			return null;
-		}
-		int length = originalString.length();
-		int beginIndex = 0;
-		int endIndex = originalString.indexOf(stringToReplace, beginIndex);
-		String leftPart = "";
-		int count = 0;
+  /**
+   * Replace all occurances of stringToReplace in string originalString with replacingString.
+   * 
+   * E.g. originalString = "hello'all'" another example: = "2005 Gold 2004 Gold" stringToReplace = "\'" = "Gold" replacingString = "''" = "Guld"
+   * 
+   * returns "hello''all''" = "2005 Guld 2004 Guld"
+   */
+  public static String replaceStringInString(String originalString, String stringToReplace, String replacingString) throws Exception {
+    String methodName = "replaceStringInString(originalString=" + originalString + ", stringToReplace=" + stringToReplace + ", replacingString=" + replacingString + ")";
+    String finalString = "";
+    if (originalString == null) {
+      return null;
+    }
+    int length = originalString.length();
+    int beginIndex = 0;
+    int endIndex = originalString.indexOf(stringToReplace, beginIndex);
+    String leftPart = "";
+    int count = 0;
 
-		if (endIndex < 0) {
-			return originalString;
-		}
+    if (endIndex < 0) {
+      return originalString;
+    }
 
-		while (beginIndex < length && endIndex >= 0) {
+    while (beginIndex < length && endIndex >= 0) {
 
-			endIndex = originalString.indexOf(stringToReplace, beginIndex);
-			if (endIndex < 0) {
-				leftPart = originalString.substring(beginIndex, length);
-				finalString = finalString + leftPart;
-			} else {
-				leftPart = originalString.substring(beginIndex, endIndex); // hello
-				finalString = finalString + leftPart + replacingString; // hello''
-			}
+      endIndex = originalString.indexOf(stringToReplace, beginIndex);
+      if (endIndex < 0) {
+        leftPart = originalString.substring(beginIndex, length);
+        finalString = finalString + leftPart;
+      } else {
+        leftPart = originalString.substring(beginIndex, endIndex); // hello
+        finalString = finalString + leftPart + replacingString; // hello''
+      }
 
-			if (endIndex + 1 >= length) {
-				return finalString;
-			} else {
-				beginIndex = endIndex + stringToReplace.length();
-			}
+      if (endIndex + 1 >= length) {
+        return finalString;
+      } else {
+        beginIndex = endIndex + stringToReplace.length();
+      }
 
-			count++;
-			if (count > 10000) {
-				// something is wrong
-				String s = "className=" + CLASS_NAME + methodName + ", caught in an endless loop";
-				logger.error(s);
-				throw new Exception(s);
-			}
-		}
-		return finalString;
-	}
+      count++;
+      if (count > 10000) {
+        // something is wrong
+        String s = "className=" + CLASS_NAME + methodName + ", caught in an endless loop";
+        logger.error(s);
+        throw new Exception(s);
+      }
+    }
+    return finalString;
+  }
 
-	/**
-	 * This method chops up a delimeitered string and puts each piece in the
-	 * resulting ArrayList Empty strings are removed.
-	 * 
-	 * @param list
-	 *            This list is allocated outside the call and filled with data
-	 *            E.g. The list get�s two new entries list[0]=Hello list[1]=Guys
-	 * @param inputString
-	 *            E.g "Hello$Guys$ $"
-	 * @param delimiter
-	 *            E.g."$"
-	 * @return
-	 */
-	public static List<String> chopUpStringToList(List<String> list, String inputString, String delimiter) {
-		if (Evaluator.isEmpty(inputString) || delimiter == null || delimiter.length() == 0) {
-			return list;
-		}
-		int length = inputString.length();
-		int beginIndex = 0;
-		int endIndex = inputString.indexOf(delimiter, beginIndex);
-		String leftPart = "";
-		int delimiter_length = delimiter.length();
+  /**
+   * This method chops up a delimeitered string and puts each piece in the resulting ArrayList Empty strings are removed.
+   * 
+   * @param list This list is allocated outside the call and filled with data E.g. The list get�s two new entries list[0]=Hello list[1]=Guys
+   * @param inputString E.g "Hello$Guys$ $"
+   * @param delimiter E.g."$"
+   * @return
+   */
+  public static List<String> chopUpStringToList(List<String> list, String inputString, String delimiter) {
+    if (Evaluator.isEmpty(inputString) || delimiter == null || delimiter.length() == 0) {
+      return list;
+    }
+    int length = inputString.length();
+    int beginIndex = 0;
+    int endIndex = inputString.indexOf(delimiter, beginIndex);
+    String leftPart = "";
+    int delimiter_length = delimiter.length();
 
-		if (endIndex < 0) {
-			// there was no delimiter found
-			list.add(inputString);
-			return list;
-		}
+    if (endIndex < 0) {
+      // there was no delimiter found
+      list.add(inputString);
+      return list;
+    }
 
-		while (beginIndex < length && endIndex >= 0) {
-			endIndex = inputString.indexOf(delimiter, beginIndex);
-			if (endIndex < 0) {
-				leftPart = inputString.substring(beginIndex, length);
-				if (!Evaluator.isEmpty(leftPart)) {
-					list.add(leftPart);
-				}
-				return list;
-			} else {
-				leftPart = inputString.substring(beginIndex, endIndex); // hello
-				if (!Evaluator.isEmpty(leftPart)) {
-					list.add(leftPart);
-				}
-			}
+    while (beginIndex < length && endIndex >= 0) {
+      endIndex = inputString.indexOf(delimiter, beginIndex);
+      if (endIndex < 0) {
+        leftPart = inputString.substring(beginIndex, length);
+        if (!Evaluator.isEmpty(leftPart)) {
+          list.add(leftPart);
+        }
+        return list;
+      } else {
+        leftPart = inputString.substring(beginIndex, endIndex); // hello
+        if (!Evaluator.isEmpty(leftPart)) {
+          list.add(leftPart);
+        }
+      }
 
-			if (endIndex + 1 >= length) {
-				return list;
-			} else {
-				beginIndex = endIndex + delimiter_length;
-			}
-		}
-		return list;
-	}
+      if (endIndex + 1 >= length) {
+        return list;
+      } else {
+        beginIndex = endIndex + delimiter_length;
+      }
+    }
+    return list;
+  }
 
-	/**
-	 * Concatenates an arbitrary amount of strings into a nicely formatted
-	 * concatenated string. Skips empty stringPart elements.
-	 * 
-	 * @param stringParts
-	 * @return
-	 */
-	public static String concatenate(String... stringParts) {
-		StringBuilder concatenatedString = new StringBuilder();
-		if (stringParts != null) {
+  /**
+   * Concatenates an arbitrary amount of strings into a nicely formatted concatenated string. Skips empty stringPart elements.
+   * 
+   * @param stringParts The strings to concatenate.
+   * @return The concatenated strings.
+   */
+  public static String concatenate(String... stringParts) {
+    StringBuilder concatenatedString = new StringBuilder();
+    if (stringParts != null) {
 
-			List<String> stringPartsList = new ArrayList<String>(Arrays.asList(stringParts));
-			for (Iterator<String> iterator = stringPartsList.iterator(); iterator.hasNext();) {
-				String stringPart = iterator.next();
-				if ("".equals(stringPart)) {
-					iterator.remove();
-				}
-			}
+      List<String> stringPartsList = new ArrayList<String>(Arrays.asList(stringParts));
+      concatenatedString.append(Formatter.concatenate(stringPartsList));
+    }
+    return concatenatedString.toString();
+  }
 
-			for (int i = 0; i < stringPartsList.size(); i++) {
-				concatenatedString.append(stringPartsList.get(i));
-				if (i < stringPartsList.size() - 1) {
-					concatenatedString.append(", ");
-				}
-			}
-		}
-		return concatenatedString.toString();
-	}
+  /**
+   * Concatenates an arbitrary amount of strings into a nicely formatted concatenated string. Skips empty stringPart elements.
+   * 
+   * @param stringParts The list of strings to concatenate.
+   * @return The concatenated strings.
+   */
+  public static String concatenate(List<String> stringParts) {
+    StringBuilder concatenatedString = new StringBuilder();
+    if (stringParts != null) {
 
+      List<String> stringPartsList = new ArrayList<String>(stringParts);
+      for (Iterator<String> iterator = stringPartsList.iterator(); iterator.hasNext();) {
+        String stringPart = iterator.next();
+        if ("".equals(stringPart)) {
+          iterator.remove();
+        }
+      }
+
+      for (int i = 0; i < stringPartsList.size(); i++) {
+        concatenatedString.append(stringPartsList.get(i));
+        if (i < stringPartsList.size() - 1) {
+          concatenatedString.append(", ");
+        }
+      }
+    }
+    return concatenatedString.toString();
+  }
 }
