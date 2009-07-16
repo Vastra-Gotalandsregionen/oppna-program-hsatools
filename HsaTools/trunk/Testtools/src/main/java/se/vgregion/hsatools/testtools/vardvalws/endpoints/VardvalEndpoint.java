cdfbdbf9069eb6142d47241e6414af153fb2a567
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-07-16 14:27:03 +0000

    Added first version of HsaTools Testtools. Testtools contains test implementations of Signicat and Vardval services suitable for performance testing etc. /David & Joakim

---
+++ HsaTools/trunk/Testtools/src/main/java/se/vgregion/hsatools/testtools/vardvalws/endpoints/VardvalEndpoint.java
@@ -0,0 +1,70 @@
+package se.vgregion.hsatools.testtools.vardvalws.endpoints;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import org.springframework.oxm.Marshaller;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.ws.server.endpoint.AbstractMarshallingPayloadEndpoint;
+
+import se.vgregion.hsatools.testtools.services.Service;
+import se.vgregion.kivtools.testservice.vardvalws.ws.domain.GetVårdvalRequest;
+import se.vgregion.kivtools.testservice.vardvalws.ws.domain.GetVårdvalResponse;
+import se.vgregion.kivtools.testservice.vardvalws.ws.domain.ObjectFactory;
+import se.vgregion.kivtools.testservice.vardvalws.ws.domain.SetVårdvalRequest;
+import se.vgregion.kivtools.testservice.vardvalws.ws.domain.SetVårdvalResponse;
+import se.vgregion.kivtools.testservice.vardvalws.ws.domain.VårdvalEntry;
+
+public class VardvalEndpoint extends AbstractMarshallingPayloadEndpoint {
+
+  private ObjectFactory objectFactory = new ObjectFactory();
+  
+  public VardvalEndpoint(Marshaller marshaller, Unmarshaller unmarshaller) {
+    super(marshaller, unmarshaller);
+  }
+  
+  @Override
+  protected Object invokeInternal(Object requestObject) throws Exception {
+    Object response = null;
+    String ssn = null;
+    String unitId = null;
+    if (requestObject instanceof GetVårdvalRequest) {
+      GetVårdvalRequest vardvalRequest = (GetVårdvalRequest) requestObject;
+      ssn = vardvalRequest.getPersonnummer().getValue();
+      unitId = Service.getUnit(ssn);
+      GetVårdvalResponse vardvalResponse = new GetVårdvalResponse();
+      VårdvalEntry vardvalEntry = objectFactory.createVårdvalEntry();
+      vardvalEntry.setVårdcentralHsaId(unitId);
+      vardvalResponse.setAktivtVårdval(objectFactory.createVårdvalEntry(vardvalEntry));
+      vardvalResponse.setKommandeVårdval(objectFactory.createVårdvalEntry(vardvalEntry));
+      response = vardvalResponse;
+    } else if (requestObject instanceof SetVårdvalRequest) {
+      SetVårdvalRequest setVardvalrequest = (SetVårdvalRequest) requestObject;
+      ssn = setVardvalrequest.getPersonnummer().getValue();
+      unitId = setVardvalrequest.getVårdcentralHsaId().getValue();
+      String currentUnitId = Service.getUnit(ssn);
+      Service.setUnit(ssn, unitId);
+
+      XMLGregorianCalendar xmlGregorianCalendar = DatatypeFactory.newInstance().newXMLGregorianCalendar((GregorianCalendar) Calendar.getInstance());
+      VårdvalEntry currentVardvalEntry = objectFactory.createVårdvalEntry();
+      currentVardvalEntry.setPersonnummer(ssn);
+      currentVardvalEntry.setGiltigFrån(xmlGregorianCalendar);
+      currentVardvalEntry.setVårdcentralHsaId(currentUnitId);
+
+      VårdvalEntry upcomingVardvalEntry = objectFactory.createVårdvalEntry();
+      upcomingVardvalEntry.setPersonnummer(ssn);
+      upcomingVardvalEntry.setGiltigFrån(xmlGregorianCalendar);
+      upcomingVardvalEntry.setVårdcentralHsaId(unitId);
+
+      SetVårdvalResponse setVardvalresponse = new SetVårdvalResponse();
+      setVardvalresponse.setAktivtVårdval(objectFactory.createVårdvalEntry(currentVardvalEntry));
+      setVardvalresponse.setKommandeVårdval(objectFactory.createVårdvalEntry(upcomingVardvalEntry));
+      response = setVardvalresponse;
+    }
+    return response;
+  }
+
+}
