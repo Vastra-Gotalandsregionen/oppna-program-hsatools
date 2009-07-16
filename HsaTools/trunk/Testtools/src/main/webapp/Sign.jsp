5e17490bbe5f3d5bfd41f0a0ba2d95aec177230b
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-07-16 14:27:03 +0000

    Added first version of HsaTools Testtools. Testtools contains test implementations of Signicat and Vardval services suitable for performance testing etc. /David & Joakim

---
+++ HsaTools/trunk/Testtools/src/main/webapp/Sign.jsp
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Insert title here</title>
+</head>
+<body>
+<form action="/signicatws/SignServlet" method="post">
+<label for="ssn">ssn</label>
+<input name="ssn" type="text" value="${param.ssn}"/><br/>
+<label for="documentArtifact">documentArtifact</label>
+<input name="documentArtifact" type="text" value="${param.documentArtifact}"/><br/>
+<label for="target">target</label>
+<input name="target" type="text" value="${param.target}"/><br/>
+<input type="submit"/>
+</form>
+</body>
+</html>
---
+++ HsaTools/trunk/Testtools/src/test/java/se/vgregion/hsatools/testtools/signicatws/endpoints/SignicatEndpointTest.java
@@ -0,0 +1,57 @@
+package se.vgregion.hsatools.testtools.signicatws.endpoints;
+
+import static org.junit.Assert.*;
+
+import org.apache.commons.codec.binary.Base64;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+
+import se.vgregion.hsatools.testtools.services.Service;
+import se.vgregion.hsatools.testtools.signicatws.endpoints.SignicatEndpoint;
+import se.vgregion.kivtools.testservice.signicatws.ws.domain.RegisterDocument;
+import se.vgregion.kivtools.testservice.signicatws.ws.domain.RegisterDocumentResponse;
+import se.vgregion.kivtools.testservice.signicatws.ws.domain.RetrieveSaml;
+import se.vgregion.kivtools.testservice.signicatws.ws.domain.RetrieveSamlResponse;
+
+public class SignicatEndpointTest {
+
+  private static final String ARTIFACT_PREFIX = "artifact";
+  private SignicatEndpoint signicatEndpoint;
+  private RegisterDocument registerDocument = new RegisterDocument();
+  private RetrieveSaml retrieveSaml = new RetrieveSaml();
+  private RegisterDocumentResponse registerDocumentResponse = new RegisterDocumentResponse();
+  private Jaxb2Marshaller jaxb2Marshaller;
+  private String SSN = "197702201111";
+  
+  @Before
+  public void setup() {
+    // Only for code coverage.
+    new Service();
+    jaxb2Marshaller = new Jaxb2Marshaller();
+    jaxb2Marshaller.setContextPath("hriv.signicatws.ws.domain");
+    signicatEndpoint = new SignicatEndpoint(jaxb2Marshaller, jaxb2Marshaller);
+  }
+
+  @Test
+  public void testInvokeInternalWithRegisterDocument() throws Exception {
+    long systemTime = System.nanoTime();
+    registerDocumentResponse = (RegisterDocumentResponse) signicatEndpoint.invokeInternal(registerDocument);
+    String[] artifact = registerDocumentResponse.getRegisterDocumentReturn().split("_");
+    assertEquals(ARTIFACT_PREFIX, artifact[0]);
+    assertTrue(systemTime < Long.valueOf(artifact[1]));
+  }
+  
+  @Test
+  public void testInvokeInternalWithRetrieveSaml() throws Exception {
+    // Prepare a artifact to fetch from service for current test.
+    String artifact = "artifact_" + System.nanoTime();
+    Service.addSignature(artifact, SSN);
+    retrieveSaml.setArtifact(artifact);
+    RetrieveSamlResponse retrieveSamlResponse = (RetrieveSamlResponse) signicatEndpoint.invokeInternal(retrieveSaml);
+    byte[] decodedResponse = Base64.decodeBase64(retrieveSamlResponse.getRetrieveSamlReturn().getBytes());
+    String decodedResponseStr = new String(decodedResponse);
+    assertTrue(decodedResponseStr.contains("<AttributeValue>" + SSN + "</AttributeValue>"));
+  }
+
+}
