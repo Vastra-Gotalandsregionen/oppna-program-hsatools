8515c1293ffa3acb5f710e4c562d0a7b06fd5231
argoyle <argoyle@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-07-17 11:43:01 +0000

    Added unit test covering the other (HsaTools-Search-composite-webcomp) UnitSearchSimpleFormValidator. /Joakim

---
+++ HsaTools/trunk/Search/composites/webcomp/src/test/java/se/vgregion/kivtools/search/validation/UnitSearchSimpleFormValidatorTest.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright 2009 Västa Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ */
+package se.vgregion.kivtools.search.validation;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import se.vgregion.kivtools.search.common.Constants;
+import se.vgregion.kivtools.search.exceptions.IncorrectUserInputException;
+import se.vgregion.kivtools.search.presentation.forms.UnitSearchSimpleForm;
+import se.vgregion.kivtools.search.svc.domain.values.MunicipalityHelper;
+
+public class UnitSearchSimpleFormValidatorTest {
+
+  private UnitSearchSimpleFormValidator validator;
+  private UnitSearchSimpleForm form;
+
+  @Before
+  public void setUp() throws Exception {
+    new MunicipalityHelper().setImplResourcePath("se.vgregion.kivtools.search.svc.impl.kiv.ldap.search-composite-svc-municipalities");
+    validator = new UnitSearchSimpleFormValidator();
+    form = new UnitSearchSimpleForm();
+  }
+
+  @Test
+  public void testValidate() throws IncorrectUserInputException {
+    try {
+      validator.validate(form);
+      fail("IncorrectUserInputException expected");
+    } catch (IncorrectUserInputException e) {
+      // Expected exception
+    }
+
+    form.setSearchParamValue("a");
+    try {
+      validator.validate(form);
+      fail("IncorrectUserInputException expected");
+    } catch (IncorrectUserInputException e) {
+      // Expected exception
+    }
+
+    form.setSearchParamValue("aa");
+    assertEquals(Constants.SUCCESSFUL_OPERATION, validator.validate(form));
+
+    form.setSearchParamValue("");
+    form.setUnitName("a");
+    try {
+      assertEquals(Constants.SUCCESSFUL_OPERATION, validator.validate(form));
+      fail("IncorrectUserInputException expected");
+    } catch (IncorrectUserInputException e) {
+      // Expected exception
+    }
+
+    form.setUnitName("aa");
+    assertEquals(Constants.SUCCESSFUL_OPERATION, validator.validate(form));
+
+    form.setSearchParamValue("a");
+    form.setUnitName("a");
+    try {
+      validator.validate(form);
+      fail("IncorrectUserInputException expected");
+    } catch (IncorrectUserInputException e) {
+      // Expected exception
+    }
+  }
+}
