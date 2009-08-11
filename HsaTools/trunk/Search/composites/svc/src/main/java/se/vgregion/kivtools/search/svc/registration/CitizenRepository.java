241e82080d9a512da49f214c66636ee3e32421a5
jonas.liljenfeldt <jonas.liljenfeldt@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-08-11 06:45:13 +0000

    In some cases, we need to concatenate cn and sn in order to get full name from LDAP.

--- HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/svc/registration/CitizenRepository.java
+++ HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/svc/registration/CitizenRepository.java
@@ -14,22 +14,29 @@ import org.springframework.ldap.core.LdapTemplate;
  */
 public class CitizenRepository {
 
-  private LdapTemplate ldapTemplate;
+	private LdapTemplate ldapTemplate;
 
-  public void setLdapTemplate(LdapTemplate ldapTemplate) {
-    this.ldapTemplate = ldapTemplate;
-  }
+	public void setLdapTemplate(LdapTemplate ldapTemplate) {
+		this.ldapTemplate = ldapTemplate;
+	}
 
-  public String getCitizenNameFromSsn(String ssn) {
-    String name = (String) ldapTemplate.lookup("uid=" + ssn, new CitizenMapper());
-    return name;
-  }
+	public String getCitizenNameFromSsn(String ssn) {
+		String name = (String) ldapTemplate.lookup("uid=" + ssn, new CitizenMapper());
+		return name;
+	}
 
-  class CitizenMapper implements AttributesMapper {
-    @Override
-    public Object mapFromAttributes(Attributes attrs) throws NamingException {
-      String name = (String) attrs.get("cn").get();
-      return name;
-    }
-  }
+	class CitizenMapper implements AttributesMapper {
+		@Override
+		public Object mapFromAttributes(Attributes attrs) throws NamingException {
+			String givenName = (String) attrs.get("cn").get();
+			String surName = (String) attrs.get("sn").get();
+			String name = "";
+			if (givenName.contains(surName)) {
+				name = givenName;
+			} else {
+				name = givenName + " " + surName;
+			}
+			return name;
+		}
+	}
 }
