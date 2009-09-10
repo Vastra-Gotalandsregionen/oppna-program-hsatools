f99b9cbc4be31025ef954ffa3af2555dc4e1b203
argoyle@gmail.com <argoyle@gmail.com@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-09-10 09:56:13 +0000

    Changed the LDAP filter criteria for surname to use sn and middleName instead of sn and rsvFirstNames as it used before. This should close two user stories in PivotalTracker. /David & Joakim

--- HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/impl/hak/ldap/PersonRepositoryTest.java
+++ HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/impl/hak/ldap/PersonRepositoryTest.java
@@ -1,18 +1,30 @@
 package se.vgregion.kivtools.search.svc.impl.hak.ldap;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
+import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class PersonRepositoryTest {
 
+  private PersonRepository personRepository;
+
+  @Before
+  public void setUp() {
+    personRepository = new PersonRepository();
+  }
+
   @Test
   public void testCreateSearchPersonsFilter() throws Exception {
-    PersonRepository personRepository = new PersonRepository();
     String searchFilter = personRepository.createSearchPersonsFilter("per", "andersson", "peran901");
-    assertEquals("(&(objectclass=hkatPerson)(regionName=*peran901*)(|(givenName=*per*)(rsvFirstNames=*per*))(|(sn=*andersson*)(rsvFirstNames=*andersson*)))", searchFilter);
+    assertEquals("(&(objectclass=hkatPerson)(regionName=*peran901*)(|(givenName=*per*)(rsvFirstNames=*per*))(|(sn=*andersson*)(middleName=*andersson*)))", searchFilter);
     searchFilter = personRepository.createSearchPersonsFilter("", "", "");
     assertNull(searchFilter);
+  }
+
+  @Test
+  public void testCreateSearchPersonsFilterOlin() throws Exception {
+    String searchFilter = personRepository.createSearchPersonsFilter("", "olin", "");
+    assertEquals("(&(objectclass=hkatPerson)(|(sn=*olin*)(middleName=*olin*)))", searchFilter);
   }
 }
