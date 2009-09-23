481700178a80f2104685fa517fb1b66ddbbf368e
argoyle <argoyle@gmail.com>
2009-09-23 05:57:02 +0000

    Added new sub-project for utilities common to both SIK and HRIV as well as a new sub-project for mock-classes used for unit testing. /David & Joakim

---
+++ HsaTools/trunk/Utils/src/main/java/se/vgregion/kivtools/util/file/FileUtil.java
@@ -0,0 +1,135 @@
+/**
+ * Copyright 2009 Västa Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ */
+package se.vgregion.kivtools.util.file;
+
+import java.io.File;
+
+/**
+ * Utility for reading and writing files.
+ * 
+ * @author David Bennehult & Joakim Olsson
+ */
+public interface FileUtil {
+  /**
+   * Reads the contents of a file in the file system.
+   * 
+   * @param fileName The absolute filename for the file to read.
+   * @return The content of the file as a String.
+   * @throws FileUtilException if the file could not be read.
+   */
+  public String readFile(String fileName);
+
+  /**
+   * Reads the contents of a file in the file system.
+   * 
+   * @param file A File object for the file to read.
+   * @return The content of the file as a String.
+   * @throws FileUtilException if the file could not be read.
+   */
+  public String readFile(File file);
+
+  /**
+   * Reads an object from a file in the file system.
+   * 
+   * @param <T> The type of object to read.
+   * @param fileName The absolute filename for the file to read.
+   * @return The object read from the file.
+   * @throws FileUtilException if the file could not be read or the object in the file is of a type not assignable to the provided type.
+   */
+  public <T> T readObjectFromFile(String fileName);
+
+  /**
+   * Reads an object from a file in the file system.
+   * 
+   * @param <T> The type of object to read.
+   * @param file A File object for the file to read.
+   * @return The object read from the file.
+   * @throws FileUtilException if the file could not be read or the object in the file is of a type not assignable to the provided type.
+   */
+  public <T> T readObjectFromFile(File file);
+
+  /**
+   * Writes the provided fileContent to a file in the file system.
+   * 
+   * @param fileName The absolute filename for the file to write.
+   * @param fileContent The content to be written to the file.
+   * @throws FileUtilException if the file could not be written.
+   */
+  public void writeFile(String fileName, String fileContent);
+
+  /**
+   * Writes the provided fileContent to a file in the file system.
+   * 
+   * @param file A File object for the file to write.
+   * @param fileContent The content to be written to the file.
+   * @throws FileUtilException if the file could not be written.
+   */
+  public void writeFile(File file, String fileContent);
+
+  /**
+   * Writes the provided object to a file in the file system.
+   * 
+   * @param fileName The absolute filename for the file to write.
+   * @param object The object to write to the file.
+   * 
+   * @throws FileUtilException if the file could not be written.
+   */
+  public void writeObjectToFile(String fileName, Object object);
+
+  /**
+   * Writes the provided object to a file in the file system.
+   * 
+   * @param file A File object for the file to write.
+   * @param object The object to write to the file.
+   * 
+   * @throws FileUtilException if the file could not be written.
+   */
+  public void writeObjectToFile(File file, Object object);
+
+  /**
+   * Creates the directory with the provided fileName if it doesn't exist.
+   * 
+   * @param directoryName The name of the directory to create.
+   * @throws IllegalArgumentException if the specified directoryName is null.
+   */
+  public void createDirectoryIfNoExist(String directoryName);
+
+  /**
+   * Creates the directory for the provided File object if it doesn't exist.
+   * 
+   * @param directory The file object describing the directory to create.
+   * @throws IllegalArgumentException if the specified directory is null.
+   */
+  public void createDirectoryIfNoExist(File directory);
+
+  /**
+   * Checks if the file with the provided filename exists.
+   * 
+   * @param fileName The name of the file to check for existence.
+   * @return True if the file exists, otherwise false.
+   */
+  public boolean fileExists(String fileName);
+
+  /**
+   * Checks if the file for the provided File object exists.
+   * 
+   * @param file The file object describing the file to check for existence.
+   * @return True if the file exists, otherwise false.
+   */
+  public boolean fileExists(File file);
+}
---
+++ HsaTools/trunk/Utils/src/main/java/se/vgregion/kivtools/util/file/FileUtilException.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright 2009 Västa Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ */
+package se.vgregion.kivtools.util.file;
+
+/**
+ * Runtime exception thrown from FileUtil.
+ * 
+ * @author David Bennehult & Joakim Olsson
+ */
+public class FileUtilException extends RuntimeException {
+  private static final long serialVersionUID = -860959014068277675L;
+
+  /**
+   * Constructs a new FileUtilException.
+   */
+  public FileUtilException() {
+    super();
+  }
+
+  /**
+   * Constructs a new FileUtilException using the provided message and cause.
+   * 
+   * @param message The message to use for this exception.
+   * @param cause The Throwable that caused this exception.
+   */
+  public FileUtilException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  /**
+   * Constructs a new FileUtilException using the provided message.
+   * 
+   * @param message The message to use for this exception.
+   */
+  public FileUtilException(String message) {
+    super(message);
+  }
+
+  /**
+   * Constructs a new FileUtilException using the provided cause.
+   * 
+   * @param cause The Throwable that caused this exception.
+   */
+  public FileUtilException(Throwable cause) {
+    super(cause);
+  }
+}
