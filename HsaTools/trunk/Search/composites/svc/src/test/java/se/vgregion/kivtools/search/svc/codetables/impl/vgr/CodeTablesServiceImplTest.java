6cdc9fc3adae758e4878bdfe13c041c29ee40620
argoyle <argoyle@gmail.com>
2009-10-18 10:21:40 +0000

    Some changes to be able to add type-ahead to advanced search in SIK. /Joakim

--- HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/codetables/impl/vgr/CodeTablesServiceImplTest.java
+++ HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/codetables/impl/vgr/CodeTablesServiceImplTest.java
@@ -1,7 +1,8 @@
 package se.vgregion.kivtools.search.svc.codetables.impl.vgr;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
+
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -19,13 +20,9 @@ public class CodeTablesServiceImplTest {
 
   private static CodeTablesServiceImpl codeTablesService;
   private static String[] managementCodeAttributes = { "1;Landsting/Region", "2;Kommun", "3;Statlig", "4;Privat, vårdavtal", "5;Privat, enl lag om läkarvårdsersättning" };
-  private static String[] specialityCodeAttributes = { "1000;Europaläkare, allmänpraktiserande läkare", "10100;Kirurgi", "10101;Urologi", "10102;Barn- och ungdomskirurgi" };
-  private static String[] vgrAO3KodAttributes = { "010;Moderförvaltning;10", "020;Regionstyrelsen (ägarutskott, arkivnämnd, regiongem. förv.org- och verks);10", "030;Revisorskollegiet",
-      "040;Patientnämnd U-a, Borås, Gbg o Mariestad" };
   private static String[] hsaLanguageKnowledgeCode = { "* SWE;* Svenska", "----------------------------------", "AAR;Afar" };
   private static String[] paTitleCode = { "104510;Ledning, kultur, turism och fritid", "105010;Ledning, tekniskt arbete", "105510;Ledning, räddningstjänst" };
-  private static String[] hsaTitleCode = { "1;Doctor"};
-  private static String[] vgrCareTypeAttributes = { "01;Öppenvård", "02;Slutenvård", "03;Hemsjukvård" };
+  private static String[] hsaTitleCode = { "1;Doctor" };
   private static LDAPConnectionMock ldapConnectionMock;
 
   @Before
@@ -48,22 +45,21 @@ public class CodeTablesServiceImplTest {
     ldapSearchResultsMock = new LDAPSearchResultsMock();
     ldapSearchResultsMock.addLDAPEntry(new LDAPEntryMock("description", hsaTitleCode));
     ldapConnectionMock.addLDAPSearchResults("(cn=" + CodeTableName.HSA_TITLE.toString() + ")", ldapSearchResultsMock);
-    
-    
+
     codeTablesService.init();
   }
 
   @Test
-  public void testInit(){
+  public void testInit() {
     try {
-    ldapConnectionMock.setLdapException(new LDAPException());
-    codeTablesService.init();
-    fail("Should throw LDAPException");
-    }catch (LDAPRuntimeExcepton e) {
+      ldapConnectionMock.setLdapException(new LDAPException());
+      codeTablesService.init();
+      fail("Should throw LDAPException");
+    } catch (LDAPRuntimeExcepton e) {
       assertEquals("An error occured in communication with the LDAP server. Message: Success", e.getMessage());
     }
   }
-  
+
   @Test
   public void testLanguageKnowledgeCode() {
     assertEquals("* Svenska", codeTablesService.getValueFromCode(CodeTableName.HSA_LANGUAGE_KNOWLEDGE_CODE, "* SWE"));
@@ -73,14 +69,59 @@ public class CodeTablesServiceImplTest {
   public void testPaTitleCode() {
     assertEquals("Ledning, tekniskt arbete", codeTablesService.getValueFromCode(CodeTableName.PA_TITLE_CODE, "105010"));
   }
-  
+
   @Test
-  public void testHsaTitle(){
+  public void testHsaTitle() {
     assertEquals("Doctor", codeTablesService.getValueFromCode(CodeTableName.HSA_TITLE, "1"));
   }
-  
+
   @Test
-  public void testGetCodeFromTextValue(){
-    assertEquals("1", codeTablesService.getCodeFromTextValue(CodeTableName.HSA_MANAGEMENT_CODE, "Landsting/Region").get(0));
+  public void testGetCodeFromTextValueFullValue() {
+    List<String> codes = codeTablesService.getCodeFromTextValue(CodeTableName.HSA_MANAGEMENT_CODE, "Landsting/Region");
+    assertEquals(1, codes.size());
+    assertEquals("1", codes.get(0));
+  }
+
+  @Test
+  public void testGetCodeFromTextValueNoMatch() {
+    List<String> codes = codeTablesService.getCodeFromTextValue(CodeTableName.HSA_MANAGEMENT_CODE, "Test123Test");
+    assertEquals(0, codes.size());
+  }
+
+  @Test
+  public void testGetCodeFromTextValueSubstring() {
+    List<String> codes = codeTablesService.getCodeFromTextValue(CodeTableName.HSA_MANAGEMENT_CODE, "st");
+    assertEquals(2, codes.size());
+    assertListContentEqual(codes, "1", "3");
+  }
+
+  @Test
+  public void testGetValuesFromTextValueFullValue() {
+    List<String> codes = codeTablesService.getValuesFromTextValue(CodeTableName.HSA_MANAGEMENT_CODE, "Landsting/Region");
+    assertEquals(1, codes.size());
+    assertEquals("Landsting/Region", codes.get(0));
+  }
+
+  @Test
+  public void testGetValuesFromTextValueNoMatch() {
+    List<String> codes = codeTablesService.getValuesFromTextValue(CodeTableName.HSA_MANAGEMENT_CODE, "Test123Test");
+    assertEquals(0, codes.size());
+  }
+
+  @Test
+  public void testGetValuesFromTextValueSubstring() {
+    List<String> codes = codeTablesService.getValuesFromTextValue(CodeTableName.HSA_MANAGEMENT_CODE, "st");
+    assertEquals(2, codes.size());
+    assertListContentEqual(codes, "Landsting/Region", "Statlig");
+  }
+
+  private void assertListContentEqual(List<String> actualStrings, String... expectedStrings) {
+    for (String actualString : actualStrings) {
+      boolean found = false;
+      for (String expectedString : expectedStrings) {
+        found |= expectedString.equals(actualString);
+      }
+      assertTrue("Unexpected string '" + actualString + "'", found);
+    }
   }
 }
