13049625d37a1371bda570fbd9897dc8eb0f2406
argoyle <argoyle@gmail.com>
2009-11-12 11:29:57 +0000

    Added WSDL-file for VardvalService and updated POM's to use the new files instead of the development-server of the VardvalService-team. Also removed a lot of stuff from the Maven-profiles to simplify development environment. /Joakim

--- HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalServiceFactoryTest.java
+++ HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalServiceFactoryTest.java
@@ -1,5 +1,10 @@
 package se.vgregion.kivtools.search.svc.ws.vardval;
 
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.net.MalformedURLException;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -7,19 +12,26 @@ import se.vgregion.kivtools.search.svc.ws.domain.vardval.IVårdvalService;
 
 /**
  * Test factory method in VardvalServiceFactory
+ * 
  * @author david
- *
+ * 
  */
 public class VardvalServiceFactoryTest {
-	
-	/**
-	 * Test that the factory method returns a IVårdvalService
-	 */
-	@Test
-	public void testGetIVardvalserviceMethod(){
-		IVårdvalService vårdvalService = VardvalServiceFactory.getIVardvalservice();
-		Assert.assertNotNull(vårdvalService);
-	}
-	
+  @Test
+  public void testInstantiation() {
+    VardvalServiceFactory vardvalServiceFactory = new VardvalServiceFactory();
+    assertNotNull(vardvalServiceFactory);
+  }
 
+  /**
+   * Test that the factory method returns a IVårdvalService
+   * 
+   * @throws MalformedURLException
+   */
+  @Test
+  public void testGetIVardvalserviceMethod() throws MalformedURLException {
+    File wsdlFile = new File("../schema/src/main/resources/wsdl/VardvalService_1.wsdl");
+    IVårdvalService vårdvalService = VardvalServiceFactory.getIVardvalservice(wsdlFile.toURI().toString(), "urn:VGRegion.VGPrimärvård.Vårdval.Service", "VårdvalService");
+    Assert.assertNotNull(vårdvalService);
+  }
 }
