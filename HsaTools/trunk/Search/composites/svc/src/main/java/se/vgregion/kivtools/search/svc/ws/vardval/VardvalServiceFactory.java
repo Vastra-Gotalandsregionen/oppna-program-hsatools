ff174458b563fd087ec87fb43aaff9a824290c5a
argoyle@gmail.com <argoyle@gmail.com@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-11-12 11:29:57 +0000

    Added WSDL-file for VardvalService and updated POM's to use the new files instead of the development-server of the VardvalService-team. Also removed a lot of stuff from the Maven-profiles to simplify development environment. /Joakim

--- HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalServiceFactory.java
+++ HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/svc/ws/vardval/VardvalServiceFactory.java
@@ -1,15 +1,31 @@
 package se.vgregion.kivtools.search.svc.ws.vardval;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import javax.xml.namespace.QName;
+
 import se.vgregion.kivtools.search.svc.ws.domain.vardval.IVårdvalService;
 import se.vgregion.kivtools.search.svc.ws.domain.vardval.VårdvalService;
+
 /**
  * Factory for generating an IVårdvalService to be used in spring context.
+ * 
  * @author David Bennehult, Jonas Liljenfeld och Joakim Olsson.
- *
+ * 
  */
 public final class VardvalServiceFactory {
 
-  public static IVårdvalService getIVardvalservice() {
-    return new VårdvalService().getBasicHttpBindingIVårdvalService();
+  /**
+   * Return the default IVårdvalService object.
+   * 
+   * @param wsdlLocation The URL to use to find the WSDL.
+   * @param namespace The namespace for the webservice.
+   * @param qNameLocalPart The local part of the Qualified Name to use for the WSDL.
+   * @return The default IVårdvalService object.
+   * @throws MalformedURLException if the provided wsdlLocation is not a valid URL.
+   */
+  public static IVårdvalService getIVardvalservice(String wsdlLocation, String namespace, String qNameLocalPart) throws MalformedURLException {
+    return new VårdvalService(new URL(wsdlLocation), new QName(namespace, qNameLocalPart)).getBasicHttpBindingIVårdvalService();
   }
 }
