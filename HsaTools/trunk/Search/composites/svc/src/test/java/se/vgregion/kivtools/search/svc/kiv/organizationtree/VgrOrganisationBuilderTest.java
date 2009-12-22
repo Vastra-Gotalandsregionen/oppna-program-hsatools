206b153227d3670c81ff78fb63f06079ba43a76d
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2009-12-22 12:46:18 +0000

    Added functionality to generate vgr organization tree structure. /David & Anders

---
+++ HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/kiv/organizationtree/VgrOrganisationBuilderTest.java
@@ -0,0 +1,82 @@
+package se.vgregion.kivtools.search.svc.kiv.organizationtree;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ldap.core.LdapTemplate;
+import org.springframework.ldap.filter.AndFilter;
+import org.springframework.ldap.filter.EqualsFilter;
+
+import se.vgregion.kivtools.search.domain.Unit;
+
+public class VgrOrganisationBuilderTest {
+
+    private VgrOrganisationBuilder vgrOrganisationBuilder;
+    private List<UnitComposition<Unit>> vgrUnitCompositions;
+
+    @Autowired
+    private LdapTemplate ldapTemplate;
+
+    @Before
+    public void setUp() throws Exception {
+        vgrOrganisationBuilder = new VgrOrganisationBuilder();
+
+        Unit unit1 = new Unit();
+        unit1.setHsaIdentity("unit1");
+        VgrUnitComposition vgrUnitComposition1 = new VgrUnitComposition("ou=node1", unit1);
+
+        Unit unit2 = new Unit();
+        unit2.setHsaIdentity("unit2");
+        VgrUnitComposition vgrUnitComposition2 = new VgrUnitComposition("ou=node2,ou=node1", unit2);
+
+        Unit unit3 = new Unit();
+        unit3.setHsaIdentity("unit3");
+        VgrUnitComposition vgrUnitComposition3 = new VgrUnitComposition("ou=node3", unit3);
+
+        Unit unit4 = new Unit();
+        unit4.setHsaIdentity("unit4");
+        VgrUnitComposition vgrUnitComposition4 = new VgrUnitComposition("ou=node4,ou=node3", unit4);
+
+        Unit unit5 = new Unit();
+        unit5.setHsaIdentity("unit5");
+        VgrUnitComposition vgrUnitComposition5 = new VgrUnitComposition("ou=node5,ou=node2,ou=node1", unit5);
+
+        vgrUnitCompositions = new ArrayList<UnitComposition<Unit>>();
+        vgrUnitCompositions.add(vgrUnitComposition2);
+        vgrUnitCompositions.add(vgrUnitComposition5);
+        vgrUnitCompositions.add(vgrUnitComposition3);
+        vgrUnitCompositions.add(vgrUnitComposition1);
+        vgrUnitCompositions.add(vgrUnitComposition4);
+    }
+
+    @Test
+    public void testGenerateOrganisation() {
+        AndFilter andFilter = new AndFilter();
+        andFilter.and(new EqualsFilter("objectClass", "vgrOrganizationalUnit"));
+        // List<UnitComposition<Unit>> unitsList = ldapTemplate.search("", andFilter.encode(),
+        // SearchControls.SUBTREE_SCOPE, new VgrUnitMapper());
+        UnitComposition<Unit> generateOrganisation = vgrOrganisationBuilder
+                .generateOrganisation(vgrUnitCompositions);
+        assertNotNull(generateOrganisation);
+
+        //Use for loop because generateOrganization use Maps that are not ordered.
+        for (UnitComposition<Unit> topNodes : generateOrganisation.getChildUnits()) {
+            if (topNodes.getUnit().getHsaIdentity().equals("unit1")) {
+                assertEquals("unit2", topNodes.getChildUnits().get(0).getUnit().getHsaIdentity());
+                assertEquals("unit5", topNodes.getChildUnits().get(0).getChildUnits().get(0).getUnit().getHsaIdentity());
+            } else if (topNodes.getUnit().getHsaIdentity().equals("unit3")) {
+                assertEquals("unit4", topNodes.getChildUnits().get(0).getUnit().getHsaIdentity());
+            } else {
+                fail("Should only contain two root nodes unit1 and unit3");
+            }
+        }
+    }
+
+}
