8f42472d1e67e5e35fc6b3406ecb449f171baf65
jonas.liljenfeldt <jonas.liljenfeldt@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2010-02-18 13:21:30 +0000

    Removed password from java code

--- HsaTools/trunk/HRIV/modules/intsvc/src/test/java/se/vgregion/kivtools/hriv/intsvc/ldap/eniro/LdapEniroIntergrationTest.java
+++ HsaTools/trunk/HRIV/modules/intsvc/src/test/java/se/vgregion/kivtools/hriv/intsvc/ldap/eniro/LdapEniroIntergrationTest.java
@@ -17,52 +17,61 @@
  */
 package se.vgregion.kivtools.hriv.intsvc.ldap.eniro;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import org.junit.Test;
 import org.springframework.ldap.core.LdapTemplate;
 import org.springframework.ldap.core.support.LdapContextSource;
 
 import se.vgregion.kivtools.hriv.intsvc.ws.eniro.FtpClient;
 import se.vgregion.kivtools.hriv.intsvc.ws.eniro.InformationPusherEniro;
-import se.vgregion.kivtools.search.domain.values.HealthcareTypeConditionHelper;
 
 public class LdapEniroIntergrationTest {
 
   /**
    * @param args
-   * @throws Exception 
+   * @throws Exception
    */
   public static void main(String[] args) throws Exception {
-    new HealthcareTypeConditionHelper().setImplResourcePath("se.vgregion.kivtools.search.svc.impl.kiv.ldap.search-composite-svc-healthcare-type-conditions");
-     LdapContextSource ldapContextSource = new LdapContextSource();
-     ldapContextSource.setUrl("ldap://kivldap01.vgregion.se:389");
-     ldapContextSource.setBase("ou=Org,o=vgr");
-     ldapContextSource.setUserDn("cn=sokso1,ou=Resurs,o=VGR");
-     ldapContextSource.setPassword("6wuz8zab");
-     ldapContextSource.setPooled(true);
-     ldapContextSource.afterPropertiesSet();
-     LdapTemplate ldapTemplate = new LdapTemplate(ldapContextSource);
-     InformationPusherEniro informationPusherEniro = new InformationPusherEniro();
-     ldapTemplate.afterPropertiesSet();
-     informationPusherEniro.setLdapTemplate(ldapTemplate);
-     ftpClientMock clientMock = new LdapEniroIntergrationTest().new ftpClientMock();
-     informationPusherEniro.setFtpClient(clientMock);
-     informationPusherEniro.doService();
-  }
+    ResourceBundle ldapConnectionResourceBundle = ResourceBundle.getBundle("se.vgregion.kivtools.hriv.intsvc.ldap.eniro.ldap");
+    String ldapUrl = ldapConnectionResourceBundle.getString("ldapUrl");
+    String ldapBase = ldapConnectionResourceBundle.getString("ldapBase");
 
+    ResourceBundle ldapCredentialResourceBundle = ResourceBundle.getBundle("se.vgregion.kivtools.hriv.intsvc.ldap.eniro.security");
+    String userDn = ldapCredentialResourceBundle.getString("userDn");
+    String password = ldapCredentialResourceBundle.getString("password");
+
+    LdapContextSource ldapContextSource = new LdapContextSource();
+    ldapContextSource.setUrl(ldapUrl);
+    ldapContextSource.setBase(ldapBase);
+    ldapContextSource.setUserDn(userDn);
+    ldapContextSource.setPassword(password);
+    ldapContextSource.setPooled(true);
+    ldapContextSource.afterPropertiesSet();
+    LdapTemplate ldapTemplate = new LdapTemplate(ldapContextSource);
+    InformationPusherEniro informationPusherEniro = new InformationPusherEniro();
+    ldapTemplate.afterPropertiesSet();
+    informationPusherEniro.setLdapTemplate(ldapTemplate);
+    ftpClientMock clientMock = new LdapEniroIntergrationTest().new ftpClientMock();
+    informationPusherEniro.setFtpClient(clientMock);
+    informationPusherEniro.doService();
+  }
 
   class ftpClientMock implements FtpClient {
 
     String fileContent;
-    
+
     @Override
     public boolean sendFile(String fileContent) {
       this.fileContent = fileContent;
       return true;
     }
-    
+
   }
+
   @Test
-  public void fake(){
-    
+  public void fake() {
+
   }
 }
