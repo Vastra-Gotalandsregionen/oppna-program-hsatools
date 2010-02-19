9aa616ec16d93e118e210e93d9739e42d1132bca
david.carlzon <david.carlzon@f9180754-8cbc-11de-b664-3b115b7b7a9b>
2010-02-19 16:24:39 +0000

    Added spring-ldap for vgr /David & Jonas

---
+++ HsaTools/trunk/Search/composites/svc/src/main/java/se/vgregion/kivtools/search/svc/impl/kiv/ldap/UnitLdapAttributes.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
+package se.vgregion.kivtools.search.svc.impl.kiv.ldap;
+
+public class UnitLdapAttributes {
+	
+	public static final String CN = "cn";
+    public static final String OBJECT_CLASS = "objectClass";
+    public static final String OU = "ou";
+    public static final String HSA_IDENTITY = "hsaIdentity";
+    public static final String ORGANIZATIONAL_UNITNAME_SHORT = "organizationalUnitNameShort";
+    public static final String DESCRIPTION = "description";
+    public static final String VGR_INTERNAL_DESCRIPTION = "vgrInternalDescription";
+    public static final String MAIL = "mail";
+    public static final String L = "l";
+    public static final String LABELED_URI = "labeledUri";
+    public static final String VGR_INTERNAL_SEDF_INVOICE_ADDRESS = "vgrInternalSedfInvoiceAddress";
+    public static final String vgrCareType = "vgrCareType";
+    public static final String vgrAO3kod = "vgrAO3kod";
+    public static final String HSA_BUSINESS_CLASSIFICATION_CODE = "hsaBusinessClassificationCode";
+    public static final String hsaTextPhoneNumber = "hsaTextPhoneNumber";
+    public static final String mobileTelephoneNumber = "mobileTelephoneNumber";
+    public static final String hsaSedfSwitchboardTelephoneNo = "hsaSedfSwitchboardTelephoneNo";
+    public static final String hsaTelephoneNumber = "hsaTelephoneNumber";
+    public static final String hsaSmsTelephoneNumber = "hsaSmsTelephoneNumber";
+    public static final String FACSIMILE_TELEPHONE_NUMBER = "facsimileTelephoneNumber";
+    public static final String PAGER_TELEPHONE_NUMBER = "pagerTelephoneNumber";
+    public static final String hsaPublicTelephoneNumber = "hsaPublicTelephoneNumber";
+    public static final String hsaTelephoneTime = "hsaTelephoneTime";
+    public static final String HSA_END_DATE = "hsaEndDate";
+    public static final String hsaSurgeryHours = "hsaSurgeryHours";
+    public static final String HSA_DROPIN_HOURS  = "hsaDropInHours";
+    public static final String HSA_INTERNAL_ADDRESS = "hsaInternalAddress";
+    public static final String hsaStreetAddress = "hsaStreetAddress";
+    public static final String HSA_POSTAL_ADDRESS = "hsaPostalAddress";
+    public static final String HSA_CONSIGNEE_ADDRESS = "hsaConsigneeAddress";
+    public static final String hsaSedfDeliveryAddress = "hsaSedfDeliveryAddress";
+    public static final String hsaSedfInvoiceAddress = "hsaSedfInvoiceAddress";
+    public static final String hsaUnitPrescriptionCode = "hsaUnitPrescriptionCode";
+    public static final String vgrAnsvarsnummer = "vgrAnsvarsnummer";
+    public static final String HSA_MUNICIPALITY_CODE = "hsaMunicipalityCode";
+    public static final String vgrEdiCode = "vgrEdiCode";
+    public static final String vgrEanCode = "vgrEanCode";
+    public static final String HSA_MUNICIPALITY_SECTION_NAME = "hsaMunicipalitySectionName";
+    public static final String HSA_MUNICIPALITY_SECTION_CODE = "hsaMunicipalitySectionCode";
+    public static final String HSA_COUNTY_CODE = "hsaCountyCode";
+    public static final String HSA_COUNTY_NAME = "hsaCountyName";
+    public static final String HSA_MANAGEMENT_CODE = "hsaManagementCode";
+    public static final String hsaVisitingRules = "hsaVisitingRules";
+    public static final String hsaVisitingRuleAge = "hsaVisitingRuleAge";
+    public static final String vgrTempInfo = "vgrTempInfo";
+    public static final String vgrRefInfo = "vgrRefInfo";
+    public static final String HSA_ADMINISTRATION_FORM = "hsaAdministrationForm";
+    public static final String vgrModifyTimestamp = "vgrModifyTimestamp";
+    public static final String CREATE_TIMESTAMP = "createTimeStamp";
+    public static final String hsaGeographicalCoordinates = "hsaGeographicalCoordinates";
+    public static final String hsaRoute = "hsaRoute";
+    public static final String vgrVardVal = "vgrVardVal";
+    public static final String VGR_AVTALSKOD = "vgrAvtalskod";
+    public static final String vgrLabeledURI = "vgrLabeledURI";
+    public static final String hsaVisitingHours = "hsaVisitingHours";
+    public static final String hsaVisitingRuleReferral = "hsaVisitingRuleReferral";
+
+}
--- HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/impl/kiv/ldap/UnitRepositoryHRIATest.java
+++ HsaTools/trunk/Search/composites/svc/src/test/java/se/vgregion/kivtools/search/svc/impl/kiv/ldap/UnitRepositoryHRIATest.java
@@ -19,7 +19,8 @@
 
 package se.vgregion.kivtools.search.svc.impl.kiv.ldap;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -37,69 +38,70 @@ import se.vgregion.kivtools.search.svc.impl.mock.LDAPConnectionMock;
 import se.vgregion.kivtools.search.svc.impl.mock.LdapConnectionPoolMock;
 
 public class UnitRepositoryHRIATest {
-  private LDAPConnectionMock ldapConnection;
-  private LdapConnectionPoolMock ldapConnectionPool;
-  private UnitRepositoryHRIA unitRepository;
+	private LDAPConnectionMock ldapConnection;
+	private LdapConnectionPoolMock ldapConnectionPool;
+	private UnitRepositoryHRIA unitRepository;
 
-  @Before
-  public void setUp() throws Exception {
-    ldapConnection = new LDAPConnectionMock();
-    ldapConnectionPool = new LdapConnectionPoolMock(ldapConnection);
+	@Before
+	public void setUp() throws Exception {
+		ldapConnection = new LDAPConnectionMock();
+		ldapConnectionPool = new LdapConnectionPoolMock(ldapConnection);
 
-    HealthcareTypeConditionHelper healthcareTypeConditionHelper = new HealthcareTypeConditionHelper();
-    healthcareTypeConditionHelper.setImplResourcePath("basic_healthcaretypeconditionhelper");
+		HealthcareTypeConditionHelper healthcareTypeConditionHelper = new HealthcareTypeConditionHelper();
+		healthcareTypeConditionHelper.setImplResourcePath("basic_healthcaretypeconditionhelper");
 
-    final SikSearchResultList<Unit> result = new SikSearchResultList<Unit>();
+		final SikSearchResultList<Unit> result = new SikSearchResultList<Unit>();
 
-    Unit resultUnit1 = new Unit();
-    resultUnit1.setHsaIdentity("abc-123");
-    resultUnit1.setHsaBusinessClassificationCode(Arrays.asList("1"));
-    resultUnit1.setVgrAnsvarsnummer(Arrays.asList("11223"));
+		Unit resultUnit1 = new Unit();
+		resultUnit1.setHsaIdentity("abc-123");
+		resultUnit1.setHsaBusinessClassificationCode(Arrays.asList("1"));
+		resultUnit1.setVgrAnsvarsnummer(Arrays.asList("11223"));
 
-    Unit resultUnit2 = new Unit();
-    resultUnit2.setHsaIdentity("abc-456");
-    resultUnit2.setHsaBusinessClassificationCode(Arrays.asList("1504"));
+		Unit resultUnit2 = new Unit();
+		resultUnit2.setHsaIdentity("abc-456");
+		resultUnit2.setHsaBusinessClassificationCode(Arrays.asList("1504"));
 
-    Unit resultUnit3 = new Unit();
-    resultUnit3.setHsaIdentity("SE6460000000-E000000000222");
-    resultUnit3.setHsaBusinessClassificationCode(Arrays.asList("abc"));
+		Unit resultUnit3 = new Unit();
+		resultUnit3.setHsaIdentity("SE6460000000-E000000000222");
+		resultUnit3.setHsaBusinessClassificationCode(Arrays.asList("abc"));
 
-    Unit resultUnit4 = new Unit();
-    resultUnit4.setHsaIdentity("abc-789");
-    resultUnit4.setHsaBusinessClassificationCode(Arrays.asList("1"));
-    resultUnit4.setVgrAnsvarsnummer(Arrays.asList("12345"));
+		Unit resultUnit4 = new Unit();
+		resultUnit4.setHsaIdentity("abc-789");
+		resultUnit4.setHsaBusinessClassificationCode(Arrays.asList("1"));
+		resultUnit4.setVgrAnsvarsnummer(Arrays.asList("12345"));
 
-    result.add(resultUnit1);
-    result.add(resultUnit2);
-    result.add(resultUnit3);
-    result.add(resultUnit4);
+		result.add(resultUnit1);
+		result.add(resultUnit2);
+		result.add(resultUnit3);
+		result.add(resultUnit4);
 
-    unitRepository = new UnitRepositoryHRIA() {
-      @Override
-      protected SikSearchResultList<Unit> searchUnits(String searchFilter, int searchScope, int maxResult, Comparator<Unit> sortOrder) throws KivException {
-        return result;
-      }
-    };
-    unitRepository.setLdapConnectionPool(ldapConnectionPool);
-  }
+		unitRepository = new UnitRepositoryHRIA() {
+			@Override
+			protected SikSearchResultList<Unit> searchUnits(String searchFilter, int searchScope, int maxResult, Comparator<Unit> sortOrder)
+					throws KivException {
+				return result;
+			}
+		};
+		// unitRepository.setLdapConnectionPool(ldapConnectionPool);
+	}
 
-  @Test
-  public void testSearchAdvancedUnitsMethod() throws Exception {
-    HealthcareType healthcareType = new HealthcareType();
-    healthcareType.addCondition("conditionKey", "value1,value2");
+	@Test
+	public void testSearchAdvancedUnitsMethod() throws Exception {
+		HealthcareType healthcareType = new HealthcareType();
+		healthcareType.addCondition("conditionKey", "value1,value2");
 
-    Unit searchUnit = new Unit();
-    searchUnit.setName("unitName");
-    searchUnit.setHsaMunicipalityName("Göteborg");
-    searchUnit.setHsaMunicipalityCode("10032");
-    searchUnit.setHsaIdentity("hsaId-1");
-    searchUnit.setHealthcareTypes(Arrays.asList(healthcareType));
-    searchUnit.setVgrVardVal(true);
+		Unit searchUnit = new Unit();
+		searchUnit.setName("unitName");
+		searchUnit.setHsaMunicipalityName("Göteborg");
+		searchUnit.setHsaMunicipalityCode("10032");
+		searchUnit.setHsaIdentity("hsaId-1");
+		searchUnit.setHealthcareTypes(Arrays.asList(healthcareType));
+		searchUnit.setVgrVardVal(true);
 
-    int maxResults = 10;
-    UnitNameComparator sortOrder = new UnitNameComparator();
-    SikSearchResultList<Unit> units = unitRepository.searchAdvancedUnits(searchUnit, maxResults, sortOrder, Arrays.asList(Integer.valueOf(1504)));
-    assertNotNull(units);
-    assertEquals(1, units.size());
-  }
+		int maxResults = 10;
+		UnitNameComparator sortOrder = new UnitNameComparator();
+		SikSearchResultList<Unit> units = unitRepository.searchAdvancedUnits(searchUnit, maxResults, sortOrder, Arrays.asList(Integer.valueOf(1504)));
+		assertNotNull(units);
+		assertEquals(1, units.size());
+	}
 }
